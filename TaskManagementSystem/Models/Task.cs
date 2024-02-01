4519f8bc9da3d8edd00ae8f307f5f939ba9019cc
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2024-02-01 11:11:02 +0100

    solved internal issue | currently working on view and refactoring

--- TaskManagementSystem/Models/Task.cs
+++ TaskManagementSystem/Models/Task.cs
@@ -1,16 +1,24 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace TaskManagementSystem.Models
 {
     public class Task
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string ?Title { get; set; }
+        public string ?Description { get; set; }
         public DateTime DueDate { get; set; }
-        public string Status { get; set; }
+        public string ?Status { get; set; }
+
+        // Example: Assuming SubTasks is a collection of SubTask
+        public List<SubTask> ?SubTasks { get; set; }
+    }
+
+    public class SubTask
+    {
+        public int Id { get; set; }
+        public string ?Name { get; set; }
+        // Other properties of SubTask
     }
 }
